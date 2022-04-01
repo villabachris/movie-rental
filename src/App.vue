f7351f076e2ba350b885de35a9df84037ac370ec
villabachris <villabachristopher@gmail.com>
2022-04-01 11:02:42 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Movies />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Movies from './components/Movies'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Movies
   }
 }
 </script>
---
+++ src/components/Movies.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Movies</h1>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: 'Movies'
+}
+
+</script>
